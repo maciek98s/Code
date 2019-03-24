8d7f90c1f76fdc33fc0f3382675a2094134aad30
maciek <31696281+maciek98s@users.noreply.github.com>
2019-03-24 13:08:25 +0000

    Update DBConnect.java

--- Java project/Java Assignment/src/com/java/assignment/DBConnect.java
+++ Java project/Java Assignment/src/com/java/assignment/DBConnect.java
@@ -15,10 +15,10 @@ public class DBConnect
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","mac","mac");
 			Statement stmt = connect.createStatement();
-			ResultSet rs = stmt.executeQuery("Select * from PARKS_IN_GALWAY_CITY");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM PARKS_IN_GALWAY_CITY");
 			while(rs.next())
 			{
-				System.out.println(rs.getStatement());
+				System.out.println(rs.getString("NUMBER"));
 			}
 			System.out.println("Connection successfull");
 		}
