886159b35f5bb6c2b4f65e3b01aa9e5115fc3be8
maciek <31696281+maciek98s@users.noreply.github.com>
2019-04-01 22:35:58 +0100

    Update Control.java

--- Java project/Java Assignment/src/com/java/assignment/Control.java
+++ Java project/Java Assignment/src/com/java/assignment/Control.java
@@ -12,10 +12,7 @@ public class Control {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//DBConnect dbc = new DBConnect();
-		//SQLqueries query1 = new SQLqueries("SELECT NAME FROM PARKS_IN_GALWAY_CITY");
-		//query1.executeQuery();
-		//query1.displayQuery();
+
 		
 		GUI gui = new GUI("DATa explorer");
 	}
