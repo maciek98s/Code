7285ec565db776b04be96641a4221374e0132f2f
maciek <31696281+maciek98s@users.noreply.github.com>
2019-04-07 23:14:04 +0100

    Update DBConnect.java

--- Java project/Java Assignment/src/com/java/assignment/DBConnect.java
+++ Java project/Java Assignment/src/com/java/assignment/DBConnect.java
@@ -10,6 +10,14 @@ public class DBConnect
 	{
 		try
 		{
+			/*  String servername = "ferdia.cs.dit.ie";
+    String portnumber = "1521";
+    String sid = "ORA11GDB";
+    String url = "jdbc:oracle:thin:@" + servername + ":" + portnumber + ":" + sid;
+    String user, pass;
+	user = readEntry("userid  : ");
+	pass = readEntry("password: ");
+	*/
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			setConnect(DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","mac","mac"));
 		}
