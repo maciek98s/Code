ca28284dfc141e7312b947e7cf275168198ca29f
maciek <31696281+maciek98s@users.noreply.github.com>
2019-04-07 23:14:06 +0100

    Update GUI.java

--- Java project/Java Assignment/src/com/java/assignment/GUI.java
+++ Java project/Java Assignment/src/com/java/assignment/GUI.java
@@ -13,37 +13,69 @@ public class GUI extends JFrame implements ActionListener
 	private JButton button1;
 	private JButton button2;
 	private JButton button3;
+	private JButton button4;
+	private JButton button5;
+	private JButton button6;
+	
 	private JTextField textfield1;
+	private JTextField textfield2;
+	private JTextField textfield3;
+	private JTextField textfield4;
+	
+	private JLabel label1;
+	
 	private JPanel panel1;
 	private JPanel panel2;
+	private JPanel panel3;
+	
 	private JTable table;
 	private DefaultTableModel model;
 	private JScrollPane pane;
 	private DefaultTableModel dataModel;
+	private JComboBox box;
+	private Queries sql;
 	
 	public GUI(String title)
 	{
 		super(title);
 		setLayout(new BorderLayout());
-		//setLayout(new FlowLayout());
+		String[] facts = {"Interesting fact 1","Interesting fact 2","Interesting fact 3","Interesting fact 4"};
+		box = new JComboBox(facts);
 		panel1 = new JPanel();
 		panel2 = new JPanel();
-		button1 = new JButton("Submit");
-		button2 = new JButton("Show");
-		button3 = new JButton("Clear");
-		textfield1 = new JTextField("Enter query here");
+		panel3 = new JPanel();
+		
+		label1 = new JLabel();
+		
+		button1 = new JButton("Connect");
+		button2 = new JButton("AdvancedMode");
+		button3 = new JButton("SQL MODE");
+		button4 = new JButton("INFO");
+		button5 = new JButton("Submit");
+		button6 = new JButton("NormalMode");
+		textfield1 = new JTextField("Enter Category");
+		
+		textfield2 = new JTextField("",5);
+		textfield3 = new JTextField("",5);
+		textfield4 = new JTextField("",5);
 		
 		add(panel1, BorderLayout.NORTH);
 		add(panel2, BorderLayout.CENTER);
+		add(panel3, BorderLayout.WEST);
 		
-		panel1.add(textfield1);
 		panel1.add(button1);
 		panel1.add(button2);
 		panel1.add(button3);
+		panel1.add(button6);
+		
 		
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
+		button5.addActionListener(this);
+		button6.addActionListener(this);
+		box.addActionListener(this);
+		
 		
 		
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -58,13 +90,20 @@ public class GUI extends JFrame implements ActionListener
 	    {
 	    	try
 	    	{
-	    		
-	    		Queries sql = new Queries(textfield1.getText());
-		    	sql.executeQuery();
-		    	sql.displayQuery();
-		    	dataModel = (DefaultTableModel) (sql.resultSetToTableModel());
-		    	table = new JTable();
-	    		table.setModel(dataModel);
+	    		sql = new Queries();
+	    		if(textfield1.getText().equals("Enter Category"))
+	    		{
+		    		sql.executeQuery("Select * from city");
+		    		normalmodeON();
+	    		}
+	    		else
+	    		{
+	    			clearTable();
+	    			sql.executeQuery(textfield1.getText());
+	    			displayTable();
+	    		}
+	    			
+
 
 	    	}
 	    	catch(Exception ee)
@@ -75,18 +114,133 @@ public class GUI extends JFrame implements ActionListener
 	    }
 	    if (e.getSource() == button2)
 	    {
-    		pane = new JScrollPane(table);
-    		pane.setPreferredSize(new Dimension(1000,400));
-    		panel2.add(pane);
-   
-    		revalidate();
+	    	clearMode();
+	    	advancemodeON();
 	    }
 	    if (e.getSource() == button3)
 	    {
-	    	panel2.removeAll();
-	    	revalidate();
-	    	repaint();
+			clearMode();
+			sqlMode();
+
+		
 	    }
+	    if (e.getSource() == box)
+	    {
+	    	String message = (String) box.getSelectedItem();
+	    	switch(message)
+	    	{
+	    		case "Interesting fact 1":
+	    		{
+	    			label1.setText("Names of the parks that are located in Barna Road, Galway ");
+	    			sql.executeQuery("select \"NAME\",\"LOCATION\" from city where \"LOCATION\" = 'Renmore, Galway'");
+	    	    	clearTable();
+	    	    	displayTable();
+	    			break;
+	    		}
+	    		case "Interesting fact 2":
+	    		{
+	    			label1.setText("Names of the parks that are located in Barna Road, Galway ");
+	    			sql.executeQuery("select \"NAME\",\"LOCATION\" from city");
+	    	    	clearTable();
+	    	    	displayTable();
+	    			break;
+	    		}
+	    		case "Interesting fact 3":
+	    		{
+	    			label1.setText("Names of the parks that are located in Barna Road, Galway ");
+	    			sql.executeQuery("select * from city");
+	    	    	clearTable();
+	    	    	displayTable();
+	    			break;
+	    		}
+	    		case "Interesting fact 4":
+	    		{
+	    			label1.setText("Names of the parks that are located in Barna Road, Galway ");
+	    			sql.executeQuery("select NAME from city where NUMBER='1'");
+	    	    	clearTable();
+	    	    	displayTable();
+	    			break;
+	    		}
+	    	
+	    	}
+	    }
+	    if (e.getSource() == button5)
+	    {
+	    	String q1,q2,q3,q4; 
+	    	
+	    	 q1  = "\""+ textfield1.getText().toUpperCase()+"\"";
+	    	 if(textfield2.getText().equals(""))
+	    	 {
+	    		 sql.executeQuery("select "+q1+"from city");
+	    	 }
+	    	 else
+	    	 {
+	    		 q2  = "\""+ textfield2.getText().toUpperCase()+"\"";
+	    		 q3  = textfield3.getText().toUpperCase();
+	    		 q4  = "'"+ textfield4.getText()+"'";
+	    		 System.out.println("select "+q1+"from city where "+q2+q3+q4);
+	    		 sql.executeQuery("select "+q1+"from city where "+q2+q3+q4);
+	    	 }
+	    	clearTable();
+	    	displayTable();
+	    }
+	    if (e.getSource() == button6)
+	    {
+	    	clearMode();
+	    	normalmodeON();
+	    }
+		
+	}
+	
+	public void normalmodeON()
+	{
+		
+		displayTable();
+		label1.setText("Interesting facts");
+		panel3.add(label1);
+		panel3.add(box);
+		revalidate();
+	}
+	public void clearMode()
+	{
+		panel3.removeAll();
+		revalidate();
+	}
+	public void advancemodeON()
+	{
+		panel3.add(button4);
+		panel3.add(textfield1);
+		panel3.add(textfield2);
+		panel3.add(textfield3);
+		panel3.add(textfield4);
+		panel3.add(button5);
+		revalidate();
+	}
+	public void displayTable()
+	{
+    	dataModel = (DefaultTableModel) (sql.resultSetToTableModel());
+    	table = new JTable();
+		table.setModel(dataModel);
+   		pane = new JScrollPane(table);
+		pane.setPreferredSize(new Dimension(1000,400));
+		panel2.add(pane);
+		revalidate();
+	}
+	public void clearTable()
+	{
+    	panel2.removeAll();
+    	revalidate();
+    	repaint();
+		
+	}
+	public void sqlMode()
+	{
+		textfield1.setText("Enter your SQL Statement here");
+		button1 = new JButton("Submit");
+		button1.addActionListener(this);
+		panel3.add(textfield1);
+		panel3.add(button1);
+		revalidate();
 		
 	}
 
