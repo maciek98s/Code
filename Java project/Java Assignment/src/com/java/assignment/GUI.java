d83cd7f2b5ccf6dcfa4c406ab7330582b1842d28
maciek <31696281+maciek98s@users.noreply.github.com>
2019-04-10 00:06:27 +0100

    Update GUI.java

--- Java project/Java Assignment/src/com/java/assignment/GUI.java
+++ Java project/Java Assignment/src/com/java/assignment/GUI.java
@@ -1,8 +1,16 @@
+/*Author : Maciej Skrzypczynski
+ *Last Update Date: 09/04/2019
+ *
+ *Desciption : A class that contains The gui of the programm. 
+ *It contains methods that execute code depending on which button is pressed
+ *and it also contains methods that manipulate with the GUI and error checking methods 
+ * 
+ */
+
 package com.java.assignment;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-import net.proteanit.sql.DbUtils;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,6 +18,7 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame implements ActionListener
 {
+	//Attributes
 	private JButton button1;
 	private JButton button2;
 	private JButton button3;
@@ -35,14 +44,19 @@ public class GUI extends JFrame implements ActionListener
 	private JScrollPane pane;
 	private DefaultTableModel dataModel;
 	private JComboBox box;
-	private Queries sql;
+	private QueryProcessing datasetQuery;
 	
 	public GUI(String title)
 	{
 		super(title);
+		//setting layour
 		setLayout(new BorderLayout());
+		//Creating an array for Combo Box
 		String[] facts = {"Interesting fact 1","Interesting fact 2","Interesting fact 3","Interesting fact 4"};
+		
+		//Initialising Attributes 
 		box = new JComboBox(facts);
+		
 		panel1 = new JPanel();
 		panel2 = new JPanel();
 		panel3 = new JPanel();
@@ -57,25 +71,27 @@ public class GUI extends JFrame implements ActionListener
 		button6 = new JButton("NormalMode");
 		button7 = new JButton("Quit");
 		button8 = new JButton("Preview");
-		textfield1 = new JTextField("Enter Category");
 		
+		textfield1 = new JTextField("Enter Category");
 		textfield2 = new JTextField("",5);
 		textfield3 = new JTextField("",5);
 		textfield4 = new JTextField("",5);
-		
+		//add panel to layout
 		add(panel1, BorderLayout.NORTH);
 		add(panel2, BorderLayout.CENTER);
 		add(panel3, BorderLayout.WEST);
 		
+		//adding components to panel
 		panel1.add(button1);
 		
+		//adding listeners to Componenets of GUI
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
-		button7.addActionListener(this);
 		button4.addActionListener(this);
 		button5.addActionListener(this);
 		button6.addActionListener(this);
+		button7.addActionListener(this);
 		button8.addActionListener(this);
 		box.addActionListener(this);
 		
@@ -93,10 +109,11 @@ public class GUI extends JFrame implements ActionListener
 	    {
 	    	try
 	    	{
+	    		
 	    		if(textfield1.getText().equals("Enter Category"))
 	    		{
-		    		sql = new Queries();
-		    		if(sql.ConnectionStatus()==false)
+		    		datasetQuery = new QueryProcessing();
+		    		if(datasetQuery.connectionStatus()==false)
 		    		{
 		    			JOptionPane.showMessageDialog(null, "Could not successfully connect to the database");
 		    			System.exit(0);
@@ -111,13 +128,13 @@ public class GUI extends JFrame implements ActionListener
 		    			panel1.remove(button1);
 		    			panel1.add(button7);
 		    		}
-		    		sql.executeQuery("Select * from city");
 		    		normalmodeON();
 	    		}
 	    		else
 	    		{
 	    			clearTable();
-	    			sql.executeQuery(textfield1.getText());
+	    			datasetQuery.executeQuery(textfield1.getText());
+	    			errorMessageCheck();
 	    			displayTable();
 	    		}
 	    			
@@ -130,52 +147,58 @@ public class GUI extends JFrame implements ActionListener
 	    	}
 	    	
 	    }
+	    //When button2 to pressed Panel clears and goes into Advance mode method 
 	    if (e.getSource() == button2)
 	    {
 	    	clearMode();
 	    	advancemodeON();
 	    	JOptionPane.showMessageDialog(null, "Welcome to the Advnaced mode please click the INFO button for more information about this mode");
 	    }
+	  //When button2 to pressed Panel clears and goes into sql mode method 
 	    if (e.getSource() == button3)
 	    {
 			clearMode();
 			sqlMode();
-			JOptionPane.showMessageDialog(null, "Welcome to SQL Mode in this mode you can enter an sql select statement of your choice with correct syntax to be viewed in the table");
+			JOptionPane.showMessageDialog(null, "Welcome to SQL Mode in this mode you can enter an sql select statement of your choice with correct syntax to be viewed in the table (The Table name is city)");
 		
 	    }
+	    //When button4 clicked display information in a form of a pop up
 	    if (e.getSource() == button4)
 	    {
-	    	JOptionPane.showMessageDialog(null, "The Advanced mode lets you specify which perticular category you would like to view and allows you for an OPTIONAL clause "
-	    			+ "for example one could enter (Number  name = Corrib Park) to view the numberid of parks which the name Corrib Park Click PREVIEW button to Show the whole table ");
+	    	JOptionPane.showMessageDialog(null, "The Advanced mode lets you specify which perticular category you would like to view and allows you for an OPTIONAL clause ");
+	    	JOptionPane.showMessageDialog(null, "for example one could enter (Number  name = Corrib Park) to view the numberid of parks which the name Corrib Park or just name with the rest of the fields blank");
+	    	JOptionPane.showMessageDialog(null, "to just view the name collumn, Click PREVIEW button to Show the whole table and remeber when searching for a specific name SPELLING AND CAPITOL latters matter ");
 	    	
 	    	
 		
 	    }
+	    //Initilising what pops up when the user interacts with the Combo Box 
 	    if (e.getSource() == box)
 	    {
+	    	//temporary string 
 	    	String message = (String) box.getSelectedItem();
 	    	switch(message)
 	    	{
 	    		case "Interesting fact 1":
 	    		{
 	    			label1.setText("Names of the parks that are located in Barna Road, Galway ");
-	    			sql.executeQuery("select \"NAME\",\"LOCATION\" from city where \"LOCATION\" = 'Renmore, Galway'");
-	    	    	clearTable();
+	    			datasetQuery.executeQuery("select \"NAME\",\"LOCATION\" from city where \"LOCATION\" = 'Renmore, Galway'");
+	    			clearTable();
 	    	    	displayTable();
 	    			break;
 	    		}
 	    		case "Interesting fact 2":
 	    		{
 	    			label1.setText("Names and locations of Parks in Galway City ");
-	    			sql.executeQuery("select \"NAME\",\"LOCATION\" from city");
-	    	    	clearTable();
+	    			datasetQuery.executeQuery("select \"NAME\",\"LOCATION\" from city");
+	    			clearTable();
 	    	    	displayTable();
 	    			break;
 	    		}
 	    		case "Interesting fact 3":
 	    		{
 	    			label1.setText("Names of the Parks in galway city and their facilities ");
-	    			sql.executeQuery("select \"NAME\",\"FACILITIES\" from city");
+	    			datasetQuery.executeQuery("select \"NAME\",\"FACILITIES\" from city");
 	    	    	clearTable();
 	    	    	displayTable();
 	    			break;
@@ -183,7 +206,7 @@ public class GUI extends JFrame implements ActionListener
 	    		case "Interesting fact 4":
 	    		{
 	    			label1.setText("Parks that are only city parks");
-	    			sql.executeQuery("select \"NAME\",\"DESCR\" from city where \"DESCR\" = 'City Park'");
+	    			datasetQuery.executeQuery("select \"NAME\",\"DESCR\" from city where \"DESCR\" = 'City Park'");
 	    	    	clearTable();
 	    	    	displayTable();
 	    			break;
@@ -191,24 +214,26 @@ public class GUI extends JFrame implements ActionListener
 	    	
 	    	}
 	    }
+	    
 	    if (e.getSource() == button5)
 	    {
 	    	String qeuryString1,qeuryString2,qeuryString3,qeuryString4; 
-	    	
+	    	clearTable();
 	    	qeuryString1  = "\""+ textfield1.getText().toUpperCase()+"\"";
 	    	 if(textfield2.getText().equals(""))
 	    	 {
-	    		 sql.executeQuery("select "+qeuryString1+"from city");
+	    		 datasetQuery.executeQuery("select "+qeuryString1+"from city");
+	    		 errorMessageCheck();
 	    	 }
 	    	 else
 	    	 {
 	    		 qeuryString2  = "\""+ textfield2.getText().toUpperCase()+"\"";
 	    		 qeuryString3  = textfield3.getText().toUpperCase();
 	    		 qeuryString4  = "'"+ textfield4.getText()+"'";
-	    		 System.out.println("select "+qeuryString1+"from city where "+qeuryString2+qeuryString3+qeuryString4);
-	    		 sql.executeQuery("select "+qeuryString1+"from city where "+qeuryString2+qeuryString3+qeuryString4);
+	    		 datasetQuery.executeQuery("select "+qeuryString1+"from city where "+qeuryString2+qeuryString3+qeuryString4);
+	    		 errorMessageCheck();
 	    	 }
-	    	clearTable();
+	    	//clearTable();
 	    	displayTable();
 	    }
 	    if (e.getSource() == button6)
@@ -219,14 +244,15 @@ public class GUI extends JFrame implements ActionListener
 	    }
 	    if (e.getSource() == button7)
 	    {
-	    	sql.closeConnection();
+	    	datasetQuery.closeConnection();
 	    	System.exit(0);
 	    	
 	    }
 	    if (e.getSource() == button8)
 	    {
 	    	clearTable();
-	    	sql.executeQuery("Select * from city");
+	    	datasetQuery.executeQuery("Select * from city");
+	    	errorMessageCheck();
 	    	displayTable();
 	    	
 	    	
@@ -264,13 +290,21 @@ public class GUI extends JFrame implements ActionListener
 	}
 	public void displayTable()
 	{
-    	dataModel = (DefaultTableModel) (sql.resultSetToTableModel());
-    	table = new JTable();
-		table.setModel(dataModel);
-   		pane = new JScrollPane(table);
-		pane.setPreferredSize(new Dimension(800,400));
-		panel2.add(pane);
-		revalidate();
+		try
+		{
+	    	
+			dataModel = (DefaultTableModel) (datasetQuery.queryToTableModel());
+	    	table = new JTable();
+			table.setModel(dataModel);
+	   		pane = new JScrollPane(table);
+			pane.setPreferredSize(new Dimension(800,400));
+			panel2.add(pane);
+			revalidate();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Table is null");
+		}
 	}
 	public void clearTable()
 	{
@@ -289,6 +323,13 @@ public class GUI extends JFrame implements ActionListener
 		panel3.add(button1);
 		revalidate();
 		
+	}
+	public void errorMessageCheck()
+	{
+		if(datasetQuery.isQueryStatus() ==false)
+		{
+			JOptionPane.showMessageDialog(null, "Oops seems like an error has occur , please make sure your input is correct");
+		}
 	}
 
 	
