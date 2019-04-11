bcab52e85c39fb0af2f868680f8f6f1570518507
maciek98s <c17339163@mydit.ie>
2019-04-11 11:31:51 +0100

    Create Control.java

---
+++ Java (2nd year)/Lab 6/lab6/work/com/Control.java
@@ -0,0 +1,18 @@
+/****
+ * Author : Maciej Skrzypczynski
+ * Date: 06/03/2019
+ * Description: Creating a gui that allows the user to play a guessing game 
+ * 
+ */
+
+package lab6.work.com;
+
+public class Control {
+
+	public static void main(String[] args) 
+	{
+		GUI gu1 = new GUI("Lab 6 Guessing Game"); 
+
+	}
+
+}
