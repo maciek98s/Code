c58d607f2471d97bd3b5f578d9ceba2ea922731b
maciek98s <c17339163@mydit.ie>
2019-04-11 11:31:09 +0100

    Create Control.java

---
+++ Java (2nd year)/Lab 1/com/lab1/test/Control.java
@@ -0,0 +1,19 @@
+/*
+ * Control : java lab 1
+ * author : Maciej skrzypczynski
+ * Date : 31/01/2019
+ */
+package com.lab1.test;
+
+public class Control 
+{
+
+	public static void main(String[] args) 
+	{
+		Vehicle car1 = new Vehicle("bob");
+		Vehicle car2 = new Vehicle("Bob","123abc",65,"red","automatic",4 );
+		System.out.println(car2);
+
+	}
+
+}
