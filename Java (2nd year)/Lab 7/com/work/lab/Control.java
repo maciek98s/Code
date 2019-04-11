b4c50833b2d39cda1e655266fad4f8bfc7139046
maciek98s <c17339163@mydit.ie>
2019-04-11 11:31:56 +0100

    Create Control.java

---
+++ Java (2nd year)/Lab 7/com/work/lab/Control.java
@@ -0,0 +1,15 @@
+package com.work.lab;
+
+public class Control {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//Date dateofbirth = new Date(14,8,1998);
+		Employee E1 = new Employee("Eddie ","Elbow",26,5,1998,"Other",696969,"Nurse",420,1234,21,3,2019);
+		
+		System.out.println(E1);
+		
+	}
+
+}
